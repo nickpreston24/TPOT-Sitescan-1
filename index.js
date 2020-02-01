4f8afca6a5b46633a8e82dfb8adac755f241c6ca
mikepreston17 <michael.n.preston@gmail.com>
2020-02-01 16:38:07 -0600

    Added cool face API

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express = require('express');
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 9001
 
-const app = express();
+const app = express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (request, response) => response.render('pages/index'))
+    .get('/cool', (request, response) => response.send(cool()))
+    .listen(PORT, () => console.log(`listening on port ${PORT}`))
 
-const PORT = process.env.PORT || 9001;
+// app.get('/', (request, response) => {
+//     console.log(request.body);
+//     response.send(`Hello there!  Come here my friend, I won't hurt you.`)
+// })
 
-app.get('/', (request, response) => {
-    console.log(request.body);
-    response.send(`Hello there!  Come here my friend, I won't hurt you.`)
-})
-
-let server = app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+// let server = app.listen(PORT, () => {
+//     console.log(`listening on port ${PORT}`)
+// })
