de07568496a660f3b8f30819b9aeee85ba971c65
mikepreston17 <michael.n.preston@gmail.com>
2020-02-01 18:59:05 -0600

    Reinstated index.js

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 9001
 
+
+
 const app = express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .get('/', (_, response) => response.render('pages/index'))
     .get('/hello', (_, response) => response.send(`Hello there!  Come here my friend, I won't hurt you.`))
     .get('/cool', (_, response) => response.send(cool()))
+    .get('api/books', (_,response) => response.send(books))
     .listen(PORT, () => console.log(`listening on port ${PORT}`))
 
 // app.get('/', (request, response) => {
