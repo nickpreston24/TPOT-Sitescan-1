3df311c3b7f20ed0601e1ce19d6f2a450ca9d815
mikepreston17 <michael.n.preston@gmail.com>
2020-05-30 20:27:15 -0500

    Created Particle Search

--- models/neode/Paper.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "title": "string",
-    "lastModified": "string",
-    "created": "string",
-    "slug": "string",
-    "excerpt": "string",
-    "firebaseUrl": "string", //arbitrary for now
-}
--- particle-parser/wordpress/api.js
+++
@@ -1,7 +0,0 @@
-import WPAPI from 'wpapi'
-
-export const wp = new WPAPI({
-    endpoint: 'https://www.thepathoftruth.com/wp-json',
-    username: 'michael.n.preston@gmail.com',
-    password: 'Mercury2020!!' //TODO: init w/ process.env or firebase
-})
